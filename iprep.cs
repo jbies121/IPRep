65fd63e8605286408e43d91414fa1d3abb25e4e7
jbies121 <jbies121@gmail.com>
2020-08-02 01:41:44 -0500

    Finish Tutorial
    Completed REST client with basic output formatting

--- iprep.cs
+++ iprep.cs
@@ -2,34 +2,43 @@
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Text.Json;
 
 namespace iprep
 {
     class Program
     {
-        private static async Task ProcessRepositories()
+        private static async Task<List<Repository>> ProcessRepositories()
         {
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+            return repositories;
 
-            var msg = await stringTask;
-            Console.Write(msg);
+            foreach (var repo in repositories)
+                Console.WriteLine(repo.Name);
         }
-
-        public class Repository
-        {
-            public string name { get; set; }
-        }
-
         private static readonly HttpClient client = new HttpClient();
 
-        static async Task Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            await ProcessRepositories();
+            var repositories = await ProcessRepositories();
+
+            foreach (var repo in repositories)
+            {
+                Console.WriteLine(repo.Name);
+                Console.WriteLine(repo.Description);
+                Console.WriteLine(repo.GitHubHomeUrl);
+                Console.WriteLine(repo.Homepage);
+                Console.WriteLine(repo.Watchers);
+                Console.WriteLine(repo.LastPush);
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ repo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace iprep
+{
+    public class Repository
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        [JsonPropertyName("homepage")]
+        public Uri Homepage { get; set; }
+
+        [JsonPropertyName("watchers")]
+        public int Watchers { get; set; }
+
+        [JsonPropertyName("pushed_at")]
+        public DateTime LastPushUtc { get; set; }
+
+        public DateTime LastPush => LastPushUtc.ToLocalTime();
+    }
+}
